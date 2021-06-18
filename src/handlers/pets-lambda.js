cfa4111d7a3b18b912ff4a2f2cfcabcbc9895229
hatertron3000 <patrick.puente@bigcommerce.com>
2021-06-18 11:42:27 -0500

    initial commit

---
+++ src/handlers/pets-lambda.js
@@ -0,0 +1,74 @@
+const BigCommerce = require('node-bigcommerce')
+
+
+exports.petsLambdaHandler = async (event) => {
+    try {
+        const attributeName = process.env.CUSTOMER_ATTRIBUTE
+
+        const bigCommerce = new BigCommerce({
+            clientId: process.env.CLIENT_ID,
+            accessToken: process.env.TOKEN,
+            storeHash: process.env.STORE_HASH,
+            responseType: 'json',
+            apiVersion: 'v3'
+        })
+
+        const attributesData = await bigCommerce.get(`/customers/attributes?name=${attributeName}`)
+        let attributeId
+
+        if (!attributesData.data.length) {
+            console.info(`No attribute named ${attributeName} found. Creating one now.`)
+            const newAttributes = [
+                {
+                    name: attributeName,
+                    type: 'string'
+                }
+            ]
+
+            const newAttributesResponse = await bigCommerce.post(`/customers/attributes`, newAttributes)
+            attributeId = newAttributesResponse.data[0].id
+        } else {
+            attributeId = attributesData.data[0].id
+        }
+
+        const customerId = event.requestContext.authorizer.bcCustomerId
+
+        const body = [
+            {
+                attribute_id: attributeId,
+                value: event.body,
+                customer_id: parseInt(customerId)
+            }
+        ]
+
+        const data = await bigCommerce.put(`/customers/attribute-values`, body)
+
+        const response = {
+            statusCode: 200,
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        };
+
+        // All log statements are written to CloudWatch
+        console.info(JSON.stringify(response));
+
+        return response;
+    } catch (err) {
+        console.error(err)
+        const response = {
+            statusCode: 500,
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({
+                data: {
+                    error: {
+                        message: "An error occurred"
+                    }
+                }
+            })
+        }
+    }
+}
